b6c84dbd3db9c2f4afbc66af0ec336429bb078ae
RaghavaThoughts <pashamraghava414@gmail.com>
2022-02-19 17:23:05 +0530

    First Commit

---
+++ src/pages/HomePage.java
@@ -0,0 +1,13 @@
+package pages;
+
+public class HomePage {
+
+	public static void main(String[] args) {
+		
+			
+			System.out.println("Hell Home Page");
+		
+
+	}
+
+}
---
+++ src/tests/HomePageTest.java
@@ -0,0 +1,10 @@
+package tests;
+
+public class HomePageTest {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Login Page");
+	}
+
+}
